0556b62ea46f6d93b36e8f09887f8bb291677985
Volker Andres <vandres@dachcom.ch>
2016-01-19 18:59:25 +0100

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,25 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Faltranslations',
+	'description' => 'Fix for Issue https://forge.typo3.org/issues/57272',
+	'category' => 'misc',
+    'author' => 'DACHCOM.DIGITAL AG',
+    'author_email' => 'digital-development@dachcom.ch',
+    'author_company' => 'DACHCOM.DIGITAL AG',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => '',
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.9.0',
+	'constraints' => array(
+		'depends' => array(
+            'typo3' => '7.6.0-7.6.2',
+        ),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+);
